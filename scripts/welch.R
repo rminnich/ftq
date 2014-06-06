a7db46a53e304bdfbdec003366d6070e22a4a4bd
Tamara Broderick <tab@stat.berkeley.edu>
2014-06-06 00:03:06 -0700

    Welch plotting now has optional plot size args
    User can specify both horizontal and vertical
    axis limits. Argument handling is now via the
    optparse library.

--- scripts/welch.R
+++ scripts/welch.R
@@ -1,5 +1,7 @@
 # library that includes the pwelch function
-library(oce)
+suppressPackageStartupMessages(library(oce))
+# library for command line option parsing
+suppressPackageStartupMessages(library(optparse))
 
 ######################################
 ### Functions
@@ -33,44 +35,74 @@ make_jelly <- function(work_amt, avg_time_diff_ns) {
 }
 
 # plot pwelch output
-plot_jelly <- function(wout, max_freq, out_filename) {
-	# get rid of extreme outliers by taking quantiles
-	min_y = quantile(wout$spec,0.001)
-	max_y = quantile(wout$spec,0.999)
-	# but leave a little wiggle room in the plot
-	max_y = max_y * 10
-	min_y = min_y / 10
-
-	# don't overstep on the max frequency
-	max_freq = min(max_freq, max(wout$freq))
+plot_jelly <- function(wout, out_filename,
+                       min_freq, max_freq,
+                       min_spec, max_spec) {
+  ### find boundaries of plot
+  max_possible_freq = max(wout$freq)
+  if(missing(min_freq)) {
+    min_freq = 0
+  }
+  if(missing(max_freq) | (max_freq <= min_freq) | (max_freq >= max_possible_freq)) {
+    max_freq = max_possible_freq
+  }
+  if(missing(min_spec) | min_spec <= 10^(-10)) {
+    # get rid of extreme outliers by taking quantiles
+    # but divide to leave a little wiggle room in the plot
+    min_spec = as.numeric(quantile(wout$spec,0.001)) / 10
+  }
+  if(missing(max_spec) | max_spec <= min_spec) {
+    max_spec = as.numeric(quantile(wout$spec,0.999)) * 10
+  }
 	
 	# plot to pdf
 	pdf(out_filename)
 	# note logarithmic spectrum (vertical) scaling
-	plot(c(0,max_freq), c(min_y,max_y), log="y", type="n",
-		xlab="frequency", ylab="spectrum")
+	plot(c(min_freq,max_freq), c(min_spec,max_spec),
+       log="y", type="n",
+       xlab="frequency", ylab="spectrum")
 	lines(wout$freq, wout$spec)	
-	dev.off()
+	invisible(dev.off())
 }
 
 ######################################
 ### Main
 ######################################
 
-# collect command line arguments
-args = commandArgs(trailingOnly = TRUE)
-if(length(args) < 3) {
-	stop("Usage: Rscript welch.R infile_name.dat outfile_name.pdf max_frequency\n")
-}
+### collect command line arguments
+# establish optional arguments
+# "-h" and "--help" are automatically in the list
+option_list <- list(
+  make_option(c("-i", "--input"), type="character",
+    default="welch_input.dat",
+    help="Input data file"),
+  make_option(c("-o", "--output"), type="character",
+    default="welch_plot.pdf",
+    help="Output file for plotting"),
+  make_option("--xmin", type="double", default=0,
+    help=paste("Minimum frequency (horizontal axis) ",
+      "in output plot [default %default]",sep="")),
+  make_option("--xmax", type="double", default=40,
+    help=paste("Maximum frequency (horizontal axis) ",
+      "in output plot [default %default]",sep="")),
+  make_option("--ymin", type="double", default=-1,
+    help=paste("Minimum spectrum (vertical axis) ",
+      "in output plot [default adaptive]",sep="")),
+  make_option("--ymax", type="double", default=-1,
+    help=paste("Maximum spectrum (vertical axis) ",
+      "in output plot [default adaptive]",sep=""))
+)
 
-filename_in = as.character(args[1])
-filename_out = as.character(args[2])
-max_freq = as.numeric(as.character(args[3]))
+# read command line
+opt <- parse_args(OptionParser(option_list=option_list))
+  
+#max_freq = as.numeric(as.character(args[3]))
 
-# read in data
-mydata = extract_data(filename_in)
+### read in data
+mydata = extract_data(opt$input)
 
-# make welch plot
+### make welch plot
 wout = make_jelly(mydata$work_amt, mydata$avg_time_step_ns)
-plot_jelly(wout, max_freq, filename_out)
+plot_jelly(wout, opt$output, opt$xmin, opt$xmax,
+           opt$ymin, opt$ymax)
 
